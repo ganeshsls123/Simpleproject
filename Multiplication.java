09b2533e2838b27e2a3e1a4690d870cd42dacfa9
ganeshsls123 <slsganesh15@gmail.com>
2022-10-01 11:35:21 +0530

    2programs

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+package small;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=1;
+		int y=2;
+		int z=x*y;
+		System.out.println(z);
+	}
+
+}
