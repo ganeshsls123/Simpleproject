793b72dcb6d0a2108a0f95ecf987745127fd63c7
ganeshsls123 <slsganesh15@gmail.com>
2022-10-01 11:17:31 +0530

    addding of two no's

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		int a=2;
+		int b=3;
+		int c=a+b;
+		System.out.println(c);
 	}
 
 }
